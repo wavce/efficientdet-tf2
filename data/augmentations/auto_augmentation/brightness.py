1131e868181c5f00c689a59670ef7484783b4909
bail <bail@deepblueai.com>
2020-04-27 15:55:28 +0800

    delete autoaugmentation

--- data/augmentations/auto_augmentation/brightness.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-from data.auto_augmentation import blend
-
-
-def brightness(image, factor):
-    degenerate = tf.zeros_like(image)
-
-    return blend(degenerate, image, factor)
--- data/augmentations/auto_augmentation/posterize.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-
-def posterize(image, bits):
-    shift = 8 - bits
-
-    return tf.bitwise.left_shift(tf.bitwise.right_shift(image, shift), shift)
